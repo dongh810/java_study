cfde5865d303dd9ce69312231dcb9b3c04a385b2
dongh810 <baek0810@naver.com>
2023-01-13 03:29:49 +0900

    230112

---
+++ src/javastudy_230112/casting.java
@@ -0,0 +1,12 @@
+package javastudy_230112;
+
+public class casting {
+    public static void main(String[] args){
+        double d = 85.6;
+        int score = (int)d;
+
+        System.out.println("score="+score);
+        System.out.println("d="+d);
+    }
+
+}
