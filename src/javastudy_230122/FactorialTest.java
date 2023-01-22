8e076255e54773c9f486072bd0e9b49a095acdb5
dongh810 <baek0810@naver.com>
2023-01-23 02:24:32 +0900

    230122 - 기본형,참조형 매개변수와 인스턴스 관련 개념공부

---
+++ src/javastudy_230122/FactorialTest.java
@@ -0,0 +1,16 @@
+package javastudy_230122;
+
+public class FactorialTest {
+    public static void main(String args[]) {
+        int result = factorial(5);
+        System.out.println(result);
+    }
+    static int factorial(int n) {
+        int result = 0;
+        if (n==1)
+            result=1;
+        else
+            result = n*factorial(n-1);
+        return result;
+    }
+}
