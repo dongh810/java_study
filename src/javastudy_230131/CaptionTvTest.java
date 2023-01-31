13435941b1b6aac1b1687d3a5e851612a5af5fa4
dongh810 <baek0810@naver.com>
2023-02-01 05:27:34 +0900

    230131 - 상속개념, 상속과 포함관계 구분

---
+++ src/javastudy_230131/CaptionTvTest.java
@@ -0,0 +1,30 @@
+package javastudy_230131;
+//상속 개념
+class Tv{
+    boolean power;
+    int channel;
+
+    void power() { power = !power;}
+    void channelUp() { ++channel;}
+    void channelDown() { --channel;}
+
+}
+class CaptionTv extends Tv{
+    boolean caption;
+    void displayCaption(String text){
+        if (caption) {
+            System.out.println(text);
+        }
+    }
+}
+public class CaptionTvTest {
+    public static void main(String args[]){
+        CaptionTv ctv = new CaptionTv();
+        ctv.channel = 10;
+        ctv.channelUp();
+        System.out.println(ctv.channel);
+        ctv.caption=true;
+        ctv.displayCaption("Hello,World");
+        //ctv.displayCaption("Hello");
+    }
+}
